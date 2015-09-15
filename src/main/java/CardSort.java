d31011d11327ac44cc17f3343dfbbeb860570495
DeanWay <deanway93@gmail.com>
2015-09-15 14:08:01 -0400

    CardSort

---
+++ src/main/java/CardSort.java
@@ -0,0 +1,134 @@
+
+import java.util.Stack;
+
+public class CardSort
+{
+    private Stack<Integer> leftDeck;
+    private Stack<Integer> middleDeck;
+    private Stack<Integer> rightDeck;
+
+    public CardSort(Stack<Integer> leftDeck, Stack<Integer> middleDeck, Stack<Integer> rightDeck)
+    {
+        this.leftDeck = leftDeck;
+        this.middleDeck = middleDeck;
+        this.rightDeck = rightDeck;
+    }
+
+    public Stack<Integer> sort()
+    {
+        primeLists(); // O(n)
+        createListofSortedSublists(); //O(n)
+        mergeSortedSublists(); //O(n^2) ...?
+        return this.leftDeck;
+    }
+
+    /**
+     * Currently this method just removes the left pile by
+     * alternately placing cards on the other piles. This process could
+     * likely be optimized.
+     */
+    private void primeLists()
+    {
+        int i = 0;
+        while (!leftDeck.empty())
+        {
+            if (i%2 == 0) {
+                middleDeck.push(leftDeck.pop());
+            }
+            else {
+                rightDeck.push(leftDeck.pop());
+            }
+            i++;
+        }
+    }
+
+    private void createListofSortedSublists()
+    {
+        while (!(middleDeck.empty() && rightDeck.empty())) {
+            if (middleDeck.empty()){
+                middleDeck.push(rightDeck.pop());
+                if (rightDeck.empty()) {
+                    leftDeck.push(middleDeck.pop());
+                    return;
+                }
+            }
+            if (rightDeck.empty()){
+                rightDeck.push(middleDeck.pop());
+                if (middleDeck.empty()) {
+                    leftDeck.push(rightDeck.pop());
+                    return;
+                }
+            }
+            if ((compare(middleDeck, leftDeck) >= 0 && compare(rightDeck, leftDeck) >= 0)
+                    || (compare(middleDeck, leftDeck) < 0 && compare(rightDeck, leftDeck) < 0)){
+                if (compare(middleDeck, rightDeck) >= 0) {
+                    leftDeck.push(middleDeck.pop());
+                } else {
+                    leftDeck.push(rightDeck.pop());
+                }
+            } else {
+                if (compare(middleDeck, rightDeck) < 0) {
+                    leftDeck.push(middleDeck.pop());
+                } else {
+                    leftDeck.push(rightDeck.pop());
+                }
+            }
+        }
+    }
+
+    private void mergeSortedSublists()
+    {
+        if (!this.leftDeck.empty()) {
+            do {
+                this.middleDeck.push(this.leftDeck.pop());
+            } while (!this.leftDeck.empty() && this.middleDeck.peek() < this.leftDeck.peek());
+        }
+        if (!this.leftDeck.empty()) {
+            do {
+                this.rightDeck.push(this.leftDeck.pop());
+            } while (!this.leftDeck.empty() && this.rightDeck.peek() < this.leftDeck.peek());
+        }
+        if (this.rightDeck.empty())
+        {
+            while (!this.middleDeck.empty()){
+                this.leftDeck.push(this.middleDeck.pop());
+            }
+            return;
+        }
+        while (!(this.middleDeck.empty() && this.rightDeck.empty())) {
+            switch (compare(this.middleDeck, this.rightDeck)) {
+                case -1: this.leftDeck.push(this.rightDeck.pop());
+                    break;
+                case 0: this.leftDeck.push(this.rightDeck.pop());
+                    break;
+                case 1: this.leftDeck.push(this.middleDeck.pop());
+                    break;
+            }
+        }
+        mergeSortedSublists();
+    }
+
+    private static int compare(Stack<Integer> deckOne, Stack<Integer> deckTwo)
+    {
+        if (deckOne.empty() && deckTwo.empty()) {
+            throw new IllegalArgumentException("Nothing to compare");
+        }
+        if (deckOne.empty()) {
+            return -1;
+        }
+        if (deckTwo.empty()) {
+            return 1;
+        }
+        if (deckOne.peek() > deckTwo.peek()) {
+            return 1;
+        }
+        else if (deckOne.peek() == deckTwo.peek()) {
+            return 0;
+        }
+        else {
+            return -1;
+        }
+    }
+
+
+}
---
+++ src/test/java/CardSortTest.java
@@ -0,0 +1,180 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.hamcrest.CoreMatchers.*;
+
+/**
+ * Tests for {@link CardSort} sorting
+ */
+public class CardSortTest {
+
+    private Stack<Integer> leftDeck = new Stack<Integer>();
+
+    private Stack<Integer> middleDeck = new Stack<Integer>();
+
+    private Stack<Integer> rightDeck = new Stack<Integer>();
+
+    private Stack<Integer> sortedDeck = new Stack<Integer>();
+
+    @SuppressWarnings("unchecked")
+    private List<Stack<Integer>> decks = Arrays.asList(leftDeck, middleDeck, rightDeck);
+
+    @Test
+    public void testSortWithThreeCards() throws Exception {
+
+        for (int i = 0; i < 3; i++) {
+            sortedDeck.push(3-i);
+        }
+
+        leftDeck.push(1);
+        middleDeck.push(2);
+        rightDeck.push(3);
+        CardSort game = new CardSort(leftDeck, middleDeck, rightDeck);
+        game.sort();
+        Assert.assertThat(leftDeck, is(equalTo(sortedDeck)));
+
+        clearDecks();
+        leftDeck.push(1);
+        middleDeck.push(3);
+        rightDeck.push(2);
+        game = new CardSort(leftDeck, middleDeck, rightDeck);
+        game.sort();
+        Assert.assertThat(leftDeck, is(equalTo(sortedDeck)));
+
+        clearDecks();
+        leftDeck.push(2);
+        middleDeck.push(1);
+        rightDeck.push(3);
+        game = new CardSort(leftDeck, middleDeck, rightDeck);
+        game.sort();
+        Assert.assertThat(leftDeck, is(equalTo(sortedDeck)));
+
+        clearDecks();
+        leftDeck.push(2);
+        middleDeck.push(3);
+        rightDeck.push(1);
+        game = new CardSort(leftDeck, middleDeck, rightDeck);
+        game.sort();
+        Assert.assertThat(leftDeck, is(equalTo(sortedDeck)));
+
+        clearDecks();
+        leftDeck.push(3);
+        middleDeck.push(1);
+        rightDeck.push(2);
+        game = new CardSort(leftDeck, middleDeck, rightDeck);
+        game.sort();
+        Assert.assertThat(leftDeck, is(equalTo(sortedDeck)));
+
+        clearDecks();
+        leftDeck.push(3);
+        middleDeck.push(2);
+        rightDeck.push(1);
+        game = new CardSort(leftDeck, middleDeck, rightDeck);
+        game.sort();
+        Assert.assertThat(leftDeck, is(equalTo(sortedDeck)));
+    }
+
+    @Test
+    public void testSortWithSixCards() throws Exception {
+
+        for (int i = 0; i < 6; i++) {
+            sortedDeck.push(6-i);
+        }
+
+        leftDeck.push(1);
+        leftDeck.push(4);
+        middleDeck.push(2);
+        middleDeck.push(5);
+        rightDeck.push(3);
+        rightDeck.push(6);
+        CardSort game = new CardSort(leftDeck, middleDeck, rightDeck);
+        game.sort();
+        Assert.assertThat(leftDeck, is(equalTo(sortedDeck)));
+
+        clearDecks();
+        leftDeck.push(1);
+        leftDeck.push(2);
+        leftDeck.push(3);
+        leftDeck.push(4);
+        middleDeck.push(5);
+        rightDeck.push(6);
+        game = new CardSort(leftDeck, middleDeck, rightDeck);
+        game.sort();
+        Assert.assertThat(leftDeck, is(equalTo(sortedDeck)));
+    }
+
+    @Test
+    public void testSortWithEmptyDecks() throws Exception {
+
+        for (int i = 0; i < 10; i++) {
+            sortedDeck.push(10-i);
+            leftDeck.push(10-i);
+        }
+
+        CardSort game = new CardSort(leftDeck, middleDeck, rightDeck);
+        game.sort();
+        Assert.assertThat(leftDeck, is(equalTo(sortedDeck)));
+
+        clearDecks();
+        for (int i = 0; i < 10; i++) {
+            leftDeck.push(i + 1);
+        }
+        game = new CardSort(leftDeck, middleDeck, rightDeck);
+        game.sort();
+        Assert.assertThat(leftDeck, is(equalTo(sortedDeck)));
+
+        clearDecks();
+        for (int i = 0; i < 10; i++) {
+            middleDeck.push(i + 1);
+        }
+        game = new CardSort(leftDeck, middleDeck, rightDeck);
+        game.sort();
+        Assert.assertThat(leftDeck, is(equalTo(sortedDeck)));
+
+        clearDecks();
+        for (int i = 0; i < 10; i++) {
+            rightDeck.push(i + 1);
+        }
+        game = new CardSort(leftDeck, middleDeck, rightDeck);
+        game.sort();
+        Assert.assertThat(leftDeck, is(equalTo(sortedDeck)));
+    }
+
+    @Test
+    public void testSortWithHighNumberOfCards() throws Exception {
+        final int numberOfCards = 10000;
+        Stack<Integer> seed = new Stack<Integer>();
+        for (int i = 0; i < numberOfCards; i++) {
+            sortedDeck.push(numberOfCards - i);
+            seed.push(numberOfCards - i);
+        }
+        Collections.shuffle(seed);
+        for (int i = 0; !seed.empty(); i++) {
+            if (i%3 == 0) {
+                leftDeck.push(seed.pop());
+            }
+            else if (i%3 == 1) {
+                middleDeck.push(seed.pop());
+            }
+            else {
+                rightDeck.push(seed.pop());
+            }
+        }
+        CardSort game = new CardSort(leftDeck, middleDeck, rightDeck);
+        long startTime = System.nanoTime();
+        game.sort();
+        long endTime = System.nanoTime();
+        Assert.assertThat(leftDeck, is(equalTo(sortedDeck)));
+        System.out.println("Sort function took " + (double)Math.round(((endTime - startTime)/1000000000.0)*1000)/1000
+                + " seconds to sort 10000 integers");
+    }
+
+    private void clearDecks(){
+        for (Stack<Integer> deck : decks) {
+            deck.clear();
+        }
+    }
+}
